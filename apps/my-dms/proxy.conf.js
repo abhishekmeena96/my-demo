19ba1c1642d09ca53e7740aeef89f7db975603bc
Abhishek <meenaabhishek0906@gmail.com>
2026-06-16 10:28:11 +0530

    Fixed the CORS issue with browser and updated proxy.config.js & app.config.json for backend api config and ecmhost config for browser for same origin

--- apps/my-dms/proxy.conf.js
+++ apps/my-dms/proxy.conf.js
@@ -1,14 +1,14 @@
-console.log('Proxy config loaded - target: http://localhost:8080');
+console.log('Proxy config loaded - target: http://10.20.20.96:8080');
 
 module.exports = {
   '/alfresco': {
-    target: 'http://localhost:8080',
+    target: 'http://10.20.20.96:8080',
     secure: false,
     changeOrigin: true,
     logLevel: 'debug',
     onProxyReq: (request) => {
       if (request['method'] !== 'GET') {
-        request.setHeader('origin', 'http://localhost:8080');
+        request.setHeader('origin', 'http://10.20.20.96:8080');
       }
     }
   }
